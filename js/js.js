acbda043cacee7073e750eb24c5f6f20b2530542
MartijnvdLans <martijnvanderlans.00@gmail.com>
2022-03-14 12:47:50 +0100

    :white_check_mark: test van example

--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+const main = document.querySelector('main');
 const public = "1e19898c87464e239192c8bfe422f280";
 const secret = "4289fec4e962a33118340c888699438d";
 const cors = "https://cors-anywhere.herokuapp.com/"
@@ -20,3 +21,20 @@ render(data);
 .catch(err => {
 console.log(err);
 });
+
+function render(data) {
+    const results = data.results;
+    console.dir(results);
+    results.forEach((item, i) => {
+      const html = `
+              <article>
+                <h2>${item.titles[0]}</h2>
+                <p>${item.summaries ? item.summaries[0] : 'Geen samenvatting'}</p>
+                <img src="${
+                  item.coverimages ? item.coverimages[1] : 'Geen samenvatting'
+                }">
+              </article>
+            `;
+      main.insertAdjacentHTML('beforeend', html);
+    });
+  }

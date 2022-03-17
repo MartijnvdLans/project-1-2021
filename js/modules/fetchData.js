7eecc7919837d010cbd878aa9dbab5bdc336903b
MartijnvdLans <martijnvanderlans.00@gmail.com>
2022-03-17 18:52:14 +0100

    :bug: API werkt niet

--- js/modules/fetchData.js
+++ js/modules/fetchData.js
@@ -30,7 +30,7 @@ export function getDataSource() {
     const key = '03b058d877ec4276bb63dd1c6e1f3768';
     const secret = '4289fec4e962a33118340c888699438d';
     const cors = 'https://cors-anywhere.herokuapp.com/';
-    const url = `${cors}https://obaliquid.staging.aquabrowser.nl/onderwijs/api/v1/search/?q=voeding+NOT+lom.lifecycle.contribute.publisher%3Dwikipedia&authorization=${key}`;
+    const url = `${cors}https://obaliquid.staging.aquabrowser.nl/onderwijs/api/v1/search/?q=voeding+NOT+lom.lifecycle.contribute.publisher%3Dwikipedia&authorization=${key}&output=json`;
 
     const config = {
         Authorization: `Bearer ${secret}`

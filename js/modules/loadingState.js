7af2f1c0b25da4e8712a05eb5d8b6cd8e51f6643
MartijnvdLans <martijnvanderlans.00@gmail.com>
2022-03-17 13:44:38 +0100

    :ambulance: Deploy Attempt 9999

--- js/modules/loadingState.js
+++ js/modules/loadingState.js
@@ -1,9 +1,11 @@
 export function loadingState() {
     const ul = document.querySelector('ul:nth-of-type(2)');
-    const placeholder = document.querySelector('section:nth-of-type(1)')
+    const placeholder = document.querySelector('section:nth-of-type(1)');
+    const errMessage = document.querySelector('section:nth-of-type(2)');
     ul.innerHTML = "";
 
     const ulLoader = document.querySelector("ul:nth-of-type(1)");
     ulLoader.classList.remove('displayNone');
-    placeholder.classList.add('displayNone')
+    errMessage.classList.add('displayNone');
+    placeholder.classList.add('displayNone');
 }
